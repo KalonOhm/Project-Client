7ba56e2865f67c5f5835dda14871ace40546a8e0
Kalon Ohmstede <106362195+KalonOhm@users.noreply.github.com>
2023-04-15 20:36:25 -0500

    Looking at GroupService, unsure what's next

--- src/app/shared/services/group.service.ts
+++ src/app/shared/services/group.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
-const URL = "http://localhost:3000/api/v1/users";
+const URL = "http://localhost:3000/api/v1/users/collections";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
+  currentCollectionId: any = null;
   currentCollectionGroups: any = []
   currentCollectionGroupsSubject: Subject<any> = new Subject;
   detailCollectionGroupsSubject: Subject<any> = new Subject;
@@ -18,7 +19,64 @@ export class GroupService {
 
   fetchGroups() {
     const token = JSON.parse(localStorage.getItem('token'));//this.authService.getToken();
-    return this.http.get(`${URL}/groups`, {);
+    return this.http.get(`${URL}/${this.currentCollectionId}/groups`, {
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+  }
+
+  fetchGroup(id) {
+    const token = JSON.parse(localStorage.getItem('token'));//this.authService.getToken();
+    return this.http.get(`${URL}/${this.currentCollectionId}/groups/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+  }
+
+  createGroup(group) {
+    const token = JSON.parse(localStorage.getItem('token'));//this.authService.getToken();
+    return this.http.post(`${URL}/${this.currentCollectionId}/groups`, group, {
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+  }
+
+  setGroups(groups){
+    this.currentCollectionGroups = groups;
+    this.currentCollectionGroupsSubject.next(groups)
+  }
+
+  onAddGroup(group){
+    this.setGroups([...this.currentCollectionGroups, group]);
+  }
+
+  onEditGroup(editedGroup, id){
+    const token = JSON.parse(localStorage.getItem('token'));//this.authService.getToken();
+    return this.http.put(`${URL}/${this.currentCollectionId}/groups/${id}`, editedGroup, {
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+  }
+
+  editGroup(editGroup: any){
+    this.detailCollectionGroupsSubject.next(editGroup);
+    const index = this.currentCollectionGroups.findIndex((group: any) => group.id === editGroup.id);
+    this.currentCollectionGroups[index] = editGroup;
+    this.setGroups(this.currentCollectionGroups);
+    //window.location.reload();
+  }
+
+  deleteGroup(id){
+    const token = JSON.parse(localStorage.getItem('token'));//this.authService.getToken();
+    return this.http.delete(`${URL}/${this.currentCollectionId}/groups/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
   }
 
 }
--- src/app/shared/services/subgroup.service.ts
+++ src/app/shared/services/subgroup.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
+const URL = "http://localhost:3000/api/v1/users/collections";
+
 @Injectable({
   providedIn: 'root'
 })
 export class SubgroupService {
-
+  
   constructor() { }
 }
